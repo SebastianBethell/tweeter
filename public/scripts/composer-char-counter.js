934c797251e27bd9343bcecf6f5d47384625bb98
Sebastian Bethell <sebbethell@gmail.com>
2017-11-14 21:18:07 +0000

    counter now updates with chars left

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,9 @@
 
 $( document ).ready( function(){
   $( ".textareatweet" ).keyup(function() {
-  console.log( 140 - ($(this).val().length) );
+  // console.log( 140 - ($(this).val().length) );
+  // console.log($(this).closest("form").find("span").text());
+  $(this).closest("form").find("span").text(140 - ($(this).val().length));
 });
 
 });
