578a082abc1a2aa9a1581ed5e71234aeac6beec3
Sebastian Bethell <sebbethell@gmail.com>
2017-11-14 23:53:43 +0000

    added styling to tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,21 @@
 "use strict";
 
+/**
+ * nothing happens until the DOM is ready
+ * on keyup in the textarea form the counter text will equal 140 - how many chars typed
+ * if less than 0 it will change the text red
+ */
 $( document ).ready( function(){
   $( ".textareatweet" ).keyup(function() {
-  // console.log( 140 - ($(this).val().length) );
-  // console.log($(this).closest("form").find("span").text());
-  $(this).closest("form").find("span").text(140 - ($(this).val().length));
-});
+    const thisTextArea = $(this).closest("form").find("span");
+    const updateCounter = thisTextArea.text(140 - ($(this).val().length));
 
+    if (thisTextArea.text() < 0) {
+      thisTextArea.addClass("counterOver");
+      updateCounter;
+    } else {
+      thisTextArea.removeClass("counterOver");
+      updateCounter;
+    }
+  });
 });
