dd71293fbe8594bf316648d4e8e52efb7bfb9dc0
Sebastian Bethell <sebbethell@gmail.com>
2017-11-15 02:28:04 +0000

    now adds the date to tweets

---
+++ public/scripts/date-finder.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$( document ).ready( function(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd = '0'+dd
+  }
+
+  if(mm<10) {
+      mm = '0'+mm
+  }
+
+  today = mm + '/' + dd + '/' + yyyy;
+
+  $('.footer-org').text(today);
+});
